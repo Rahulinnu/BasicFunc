d8fce4dfcfa440b6edbeaac281caf04862a8fccf
rahul unni <rahul@rahulsmac.local>
2022-05-05 16:00:42 +0530

    All  added

--- EvenOrOdd.cs
+++ EvenOrOdd.cs
@@ -3,8 +3,19 @@ namespace BasicFunc
 {
     public class EvenOrOdd
     {
-        public EvenOrOdd()
+        public void EvOd()
         {
+            Console.WriteLine("Enter a number to check even or odd: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given number " + num + " Is even");
+            }
+            else
+            {
+                Console.WriteLine("The given number " + num + " Is Odd");
+            }
+            Console.ReadLine();
         }
     }
 }
--- LeapYear.cs
+++ LeapYear.cs
@@ -3,8 +3,20 @@ namespace BasicFunc
 {
     public class LeapYear
     {
-        public LeapYear()
+        public void CalcLeapYear()
         {
+            Console.WriteLine("Welcome to Leap Year Program");
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+            Console.ReadLine();
         }
     }
 }
