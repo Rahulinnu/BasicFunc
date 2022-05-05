fb519020e1c3ad9afad038c79fd8de56002d443f
rahul unni <rahul@rahulsmac.local>
2022-05-05 15:57:44 +0530

    Flip coin  added

--- FlipCoin.cs
+++ FlipCoin.cs
@@ -3,8 +3,31 @@ namespace BasicFunc
 {
     public class FlipCoin
     {
-        public FlipCoin()
+        public void CalcFlipCoin()
         {
+            Console.WriteLine("Welcome to Coin Flip Program");
+            int heads = 0;
+            int tails = 0;
+
+            Console.WriteLine("Enter the Coin flipping operation range: ");
+            int count = int.Parse(Console.ReadLine());
+            while (count > 0)
+            {
+                Random rand = new Random();
+                int randomCheck = rand.Next(2);
+                if (randomCheck == 0)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+                count--;
+            }
+            Console.WriteLine("Number of heads is: " + heads);
+            Console.WriteLine("Number of Tails is: " + tails);
+            Console.ReadLine();
         }
     }
 }
